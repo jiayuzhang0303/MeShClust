824db76668efbc1e6001ce51c8a92a1a842ff812
Zhang Jiayu's macbook <zhangjiayu@ZhangdeMacBook-Air-2.local>
2018-09-22 23:49:05 +0200

    modified Hello world

--- java/Hello.java
+++ java/Hello.java
@@ -1,6 +1,6 @@
 /* My first Java Program to say Hello. */
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("Hello,world!");
+		System.out.println("Hello world!");
 	}
 }
